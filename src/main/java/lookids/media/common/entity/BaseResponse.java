a35cfaa9ad6a7644be1e0be926fcd7ad3406c68d
PSangEon <qkr99102@naver.com>
2024-11-13 10:51:39 +0900

    Fix: BaseResponse 수정
    KAN-182

--- src/main/java/lookids/media/common/entity/BaseResponse.java
+++ src/main/java/lookids/media/common/entity/BaseResponse.java
@@ -3,9 +3,8 @@ package lookids.media.common.entity;
 import static lookids.media.common.entity.BaseResponseStatus.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
-public record BaseResponse<T>(HttpStatusCode httpStatus, Boolean isSuccess, String message, int code, T result) {
+public record BaseResponse<T>(HttpStatus httpStatus, Boolean isSuccess, String message, int code, T result) {
 
 	/**
 	 * 필요값 : Http상태코드, 성공여부, 메시지, 에러코드, 결과값
